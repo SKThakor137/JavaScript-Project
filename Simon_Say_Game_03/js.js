7dddceafc8afea41c4dd6d99c7fa68768c1c3356
SKThakor137 <shaileshthakor383430@gmail.com>
2024-02-03 16:05:10 +0530

    clear code and modify some code

--- Simon_Say_Game_03/js.js
+++ Simon_Say_Game_03/js.js
@@ -5,7 +5,7 @@ let btns = ["yellow", "red", "purple", "green"];
 let started = false;
 let level = 0;
 
-let h2 = document.querySelector("h2");
+let h3 = document.querySelector("h3");
 let body = document.querySelector("body");
 
 body.addEventListener("keypress", function () {
@@ -33,7 +33,7 @@ function userFlash(btn) {
 function levelUp() {
   userSeq = [];
   level++;
-  h2.innerHTML = `Level ${level}`;
+  h3.innerHTML = `Level ${level}`;
 
   let randIdx = Math.floor(Math.random() * btns.length);
   let randColor = btns[randIdx];
@@ -46,32 +46,17 @@ function levelUp() {
 }
 
 function checkAns(idx) {
-  // console.log("curr level : ", level);
-
-  // // let idx = level - 1;
-  // console.log("idx -> ", idx);
-  // console.log("user idx - ", userSeq[idx], "game idx - ", gameSeq[idx]);
-  // console.log(
-  //   "user length : ",
-  //   userSeq.length,
-  //   "game length : ",
-  //   gameSeq.length
-  // );
-
   if (userSeq[idx] === gameSeq[idx]) {
     if (userSeq.length == gameSeq.length) {
-      // levelUp();
       setTimeout(levelUp, 1000);
     }
-    // console.log("Same Value");
   } else {
-    // console.log("game over");
     document.querySelector("body").style.backgroundColor = "red";
     setTimeout(() => {
       document.querySelector("body").style.backgroundColor = "white";
     }, 200);
 
-    h2.innerHTML = `Game Over! Your Score was <b>${level}<b><br> Press any key to start`;
+    h3.innerHTML = `Game Over! Your Score was <b>${level}<b><br> Press any key to start`;
     let span = document.querySelector("span");
     if (level > highScore) {
       highScore = level;
@@ -82,7 +67,6 @@ function checkAns(idx) {
 }
 
 function btnPress() {
-  // console.log(this);
   let btn = this;
   userFlash(btn);
 
@@ -95,7 +79,6 @@ function btnPress() {
 
 let allBtns = document.querySelectorAll(".btn");
 for (btn of allBtns) {
-  // console.log(btn);
   btn.addEventListener("click", btnPress);
 }
 
